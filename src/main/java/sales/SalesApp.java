dfb83f6f155a30d61cb7ebb07c229da2d7c81e4a
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-08-10 19:28:30 +0800

    Ref:refactor SalesAPP.java

--- src/main/java/sales/SalesApp.java
+++ src/main/java/sales/SalesApp.java
@@ -1,57 +1,22 @@
 package sales;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 public class SalesApp {
-
+	private SalesDao salesDao;
+	private SalesReportDao salesReportDao;
 	public void generateSalesActivityReport(String salesId, int maxRow, boolean isNatTrade, boolean isSupervisor) {
 
-		SalesDao salesDao = new SalesDao();
-		SalesReportDao salesReportDao = new SalesReportDao();
-		List<String> headers = null;
+		if (salesId == null) return;
+		Sales sales = getSales(salesId);
 
-		List<SalesReportData> filteredReportDataList = new ArrayList<SalesReportData>();
+		if (isEffectiveDate(sales)) return;
 
-		if (salesId == null) {
-			return;
-		}
+		List<SalesReportData> reportDataList = getSalesReportData(sales);
 
-		Sales sales = salesDao.getSalesBySalesId(salesId);//通过销售ID拿到某
-
-		Date today = new Date();
-		if (today.after(sales.getEffectiveTo())//如果是有效日期
-				|| today.before(sales.getEffectiveFrom())){
-			return;
-		}
-
-		List<SalesReportData> reportDataList = salesReportDao.getReportData(sales);
-
-		for (SalesReportData data : reportDataList) {
-			if ("SalesActivity".equalsIgnoreCase(data.getType())) {
-				if (data.isConfidential()) {//保密的
-					if (isSupervisor) {//是主管
-						filteredReportDataList.add(data);
-					}
-				}else {
-					filteredReportDataList.add(data);
-				}
-			}
-		}
-
-		List<SalesReportData> tempList = new ArrayList<SalesReportData>();
-		for (int i=0; i < reportDataList.size() || i < maxRow; i++) {
-			tempList.add(reportDataList.get(i));
-		}
-		filteredReportDataList = tempList;
-
-		if (isNatTrade) {
-			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Time");
-		} else {
-			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Local Time");
-		}
+		List<String> headers = getHeaders(isNatTrade);
 
 		SalesActivityReport report = this.generateReport(headers, reportDataList);
 
@@ -60,7 +25,33 @@ public class SalesApp {
 
 	}
 
-	private SalesActivityReport generateReport(List<String> headers, List<SalesReportData> reportDataList) {
+	public Sales getSales(String salesId) {
+		//SalesDao salesDao = new SalesDao();//销售数据访问对象
+		return salesDao.getSalesBySalesId(salesId);//拿到sales
+	}
+
+	public boolean isEffectiveDate(Sales sales) {
+		Date today = new Date();
+		return (today.after(sales.getEffectiveTo()) || today.before(sales.getEffectiveFrom()));//如果不是有效日期
+	}
+
+	public List<SalesReportData> getSalesReportData(Sales sales) {
+		//SalesReportDao salesReportDao = new SalesReportDao();//销售报告访问对象
+		return salesReportDao.getReportData(sales);
+	}
+
+
+	public List<String> getHeaders(boolean isNatTrade) {
+		List<String> headers = null;
+		if (isNatTrade) {
+			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Time");
+		} else {
+			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Local Time");
+		}
+		return headers;
+	}
+
+	public SalesActivityReport generateReport(List<String> headers, List<SalesReportData> reportDataList) {
 		// TODO Auto-generated method stub
 		return null;
 	}
